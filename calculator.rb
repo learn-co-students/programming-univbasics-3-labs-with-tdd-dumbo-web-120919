b022fe1e6ebfaaa597ea3135ac5c31d3079581c6
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-17 10:24:31 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
 # Add your variables here
+first_number = 0
+second_number = 1
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+# def Add (first_number, second_number)
+#   sum = first_number + second_number
+# end
+
+# def difference(first_number, second_number)
+#   second_number - first_number
+# end
